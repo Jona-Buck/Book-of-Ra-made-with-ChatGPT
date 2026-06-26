9f29386a747a9d9ebf518c85c0d1009cf2a38247
Jona-Buck <jona.buck.zweitadresse@gmx.de>
2026-06-26 10:09:29 +0200

    RNG: hochvolatile Symbolgewichte (book<exp<isis<scarab, 1:160 FS)

--- js/config.js
+++ js/config.js
@@ -2,16 +2,16 @@
 const I={A:"./assets/A.webp",K:"./assets/K.webp",J:"./assets/J.webp",Q:"./assets/Q.webp",ten:"./assets/ten.webp",isis:"./assets/isis.webp",scarab:"./assets/scarab.webp",exp:"./assets/exp.webp",book:"./assets/book.webp"};
 
 const SY=[
-  {id:'book',  s:I.book,  n:'Buch (Wild/Scatter)',pay:{3:18,4:180,5:1800},w:3,fill:true},
-  {id:'exp',   s:I.exp,   n:'Abenteurer',         pay:{3:10,4:100,5:1000},w:5,fill:true},
-  {id:'isis',  s:I.isis,  n:'Isis',                pay:{3:5,4:50,5:500},  w:8},
-  {id:'scarab',s:I.scarab,n:'Skarab\u00e4us',      pay:{3:4,4:40,5:400},  w:9},
-  {id:'A',     s:I.A,     n:'Ass',                 pay:{3:2,4:20,5:200},  w:11},
-  {id:'K',     s:I.K,     n:'K\u00f6nig',          pay:{3:2,4:20,5:200},  w:12},
-  {id:'Q',     s:I.Q,     n:'Dame',                pay:{3:2,4:20,5:200},  w:14},
-  {id:'J',     s:I.J,     n:'Bube',                pay:{3:2,4:20,5:200},  w:15},
-  {id:'ten',   s:I.ten,   n:'10',                  pay:{3:2,4:20,5:200},  w:16},
-];
+  {id:'book',  s:I.book,  n:'Buch (Wild/Scatter)',pay:{3:18,4:180,5:1800},w:2, fill:true},
+  {id:'exp',   s:I.exp,   n:'Abenteurer',         pay:{3:10,4:100,5:1000},w:3, fill:true},
+  {id:'isis',  s:I.isis,  n:'Isis',                pay:{3:5,4:50,5:500},  w:4},
+  {id:'scarab',s:I.scarab,n:'Skarab\u00e4us',      pay:{3:4,4:40,5:400},  w:5},
+  {id:'A',     s:I.A,     n:'Ass',                 pay:{3:2,4:20,5:200},  w:6},
+  {id:'K',     s:I.K,     n:'K\u00f6nig',          pay:{3:2,4:20,5:200},  w:7},
+  {id:'Q',     s:I.Q,     n:'Dame',                pay:{3:2,4:20,5:200},  w:9},
+  {id:'J',     s:I.J,     n:'Bube',                pay:{3:2,4:20,5:200},  w:11},
+  {id:'ten',   s:I.ten,   n:'10',                  pay:{3:2,4:20,5:200},  w:18},
+]; /* total weight=65 | Freispiele ~1:160 | book<exp<isis<scarab (seltenste zuerst) */
 const TW=SY.reduce((a,x)=>a+x.w,0);
 function rnd(){let r=Math.random()*TW;for(const s of SY){r-=s.w;if(r<=0)return s;}return SY[SY.length-1];}
 const PL=[[1,1,1,1,1],[0,0,0,0,0],[2,2,2,2,2],[0,1,2,1,0],[2,1,0,1,2],[1,0,0,0,1],[1,2,2,2,1],[0,0,1,2,2],[2,2,1,0,0],[0,1,0,1,0]];
